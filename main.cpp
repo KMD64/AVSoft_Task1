ece9dab7ef9f2a66b6826586f08eb66d27251951
Maxwell <nintemod98@gmail.com>
2020-08-17 16:29:13 +0300

    Added some comments

--- main.cpp
+++ main.cpp
@@ -1,26 +1,29 @@
-#include <iostream>
-#include <mutex>
-#include <thread>
-#include <memory>
-#include <list>
-#include <condition_variable>
-#include <random>
-#include <sstream>
+#include <iostream>//I/O streams
+#include <mutex>//mutex
+#include <thread>//thread
+#include <memory>//shared_ptr
+#include <list>//list
+#include <random>//mt19937
 //----------------------------
-//Defines zone
-#define READERS 4
-#define WRITERS 8
+//Зона определений
+//----------------------------
+//Количество читателей
+#define READERS 8
+//Количество писателей
+#define WRITERS 5
+//Количество повторов обращения
 #define REPEATS 8
+//Задержка между обращениями
 #define DELAY_MS 500
 //----------------------------
 using namespace std;
-using namespace chrono_literals;
 
+//Блокирующий контейнер
 class concurrent_data{
     int _data;
     std::mutex _mx;
-    int reads=0;
     int writes=0;
+    int reads=0;
 public:
     concurrent_data(int i=0):_data(i){
 
@@ -29,7 +32,6 @@ public:
     {
         std::lock_guard<mutex> lock(_mx);
         _data = newData;
-
         cout<<++writes<<":"<<this_thread::get_id()<<" Write: "<<_data<<endl;
     }
     int read()
@@ -39,10 +41,9 @@ public:
         return _data;
     }
 };
-
+//Задача чтения значения
 class reader_task{
     shared_ptr<concurrent_data> _data;
-
 public:
     reader_task(concurrent_data *data):_data(data){
     }
@@ -54,10 +55,11 @@ public:
 
     }
 };
-//writes random value to data
+//Задача записи случайного значения
 class writer_task{
     shared_ptr<concurrent_data> _data;
     int _number;
+    //random number generator
     mt19937 generator;
 public:
     writer_task(concurrent_data *data):_data(data){
@@ -65,7 +67,7 @@ public:
     }
     void operator()(){
         for(int i=0;i<REPEATS;++i){
-            _number = generator()/25;
+            _number = generator()%25;
             _data->set(_number);
             this_thread::sleep_for(chrono::milliseconds(DELAY_MS));
         }
@@ -73,7 +75,9 @@ public:
 };
 int main()
 {
-    auto data = new concurrent_data(25);
+    //Общий контейнер
+    auto data = new concurrent_data(22);
+    //Заполняем список потоков
     list<unique_ptr<thread>> threads;
     for(int i=0;i<READERS;++i){
         threads.push_front(make_unique<thread>(reader_task(data)));
@@ -81,7 +85,7 @@ int main()
     for(int i=0;i<WRITERS;++i){
         threads.push_front(make_unique<thread>(writer_task(data)));
     }
-    //starting threads
+    //Запускаем потоки
     for(auto &thread:threads){
         thread->join();
     }
